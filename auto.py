10bb76de35bc5a3a635a370494c11302a59bac03
iamsk <iamsk.info@gmail.com>
2025-08-28 23:57:44 +0800

    db

--- auto.py
+++ auto.py
@@ -1,45 +1,12 @@
-import os
-from datetime import datetime
-from unittest.mock import MagicMock
-
-import questionary
-import typer
-from dotenv import load_dotenv
-
-from cli import main, utils
-
-load_dotenv()
-
-
-def run_one(ticker):
-    user_selections = {
-        "ticker": ticker,
-        "analysis_date": datetime.now().strftime("%Y-%m-%d"),
-        "analysts": [questionary.Choice(display, value=value.value) for display, value in utils.ANALYST_ORDER],
-        "research_depth": 5,
-        "llm_provider": "openai",
-        "backend_url": os.getenv('BACKEND_URL'),
-        "shallow_thinker": "gpt-5-mini",
-        "deep_thinker": "o4-mini-high",
-    }
-    # print(user_selections)
-    main.get_user_selections = MagicMock(return_value=user_selections)
-    main.run_analysis()
-
+import subprocess
+import time
 
 if __name__ == '__main__':
-    typer.run(run_one)
-    # run_one('ORCL')
-    exit()
     # get_top_stocks()
-    stocks = ['NVDA', 'MSFT', 'AAPL', 'AMZN',
-              # 'GOOG',
-              'META',
-              # 'TSLA',
-              'WMT',
-              # 'ORCL',
-              'COIN',
-              'CRCL',
-              'HOOD']
+    stocks = ['NVDA', 'BABA']
     for stock in stocks:
-        run_one(stock)
+        print(f"{time.time()}: {stock}")
+        subprocess.run(
+            ["/Users/zhangbin/workspace/TradingAgents/.venv/bin/python",
+             "/Users/zhangbin/workspace/TradingAgents/one.py",
+             stock])
---
+++ one.py
@@ -0,0 +1,40 @@
+import os
+from datetime import datetime
+from unittest.mock import MagicMock
+
+import pymongo
+import questionary
+import typer
+from dotenv import load_dotenv
+
+from cli import main, utils
+
+host = os.getenv('MONGODB_HOST')
+port = 3717
+client = pymongo.MongoClient(host, port)
+
+table = client.private.tradingagents.records
+
+load_dotenv()
+
+
+def run_one(ticker):
+    analysis_date = datetime.now().strftime("%Y-%m-%d")
+    user_selections = {
+        "ticker": ticker,
+        "analysis_date": analysis_date,
+        "analysts": [questionary.Choice(display, value=value.value) for display, value in utils.ANALYST_ORDER],
+        "research_depth": 5,
+        "llm_provider": "openai",
+        "backend_url": os.getenv('BACKEND_URL'),
+        "shallow_thinker": "gpt-4.1-mini",
+        "deep_thinker": "o4-mini",
+    }
+    # print(user_selections)
+    main.get_user_selections = MagicMock(return_value=user_selections)
+    main.run_analysis()
+    table.insert_one({'ticker': ticker, 'analysis_date': analysis_date})
+
+
+if __name__ == '__main__':
+    typer.run(run_one)
