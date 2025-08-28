3b682fc66a390e11f3be960083379cc7169a2d6b
iamsk <iamsk.info@gmail.com>
2025-08-29 01:45:51 +0800

    path

--- auto.py
+++ auto.py
@@ -1,12 +1,22 @@
 import subprocess
 import time
+from datetime import date, timedelta
+
+from mdb import client
+
+table = client.private.tradingview.最活跃
+
+today = date.today()
+yesterday = today + timedelta(days=-1)
+yesterday_str = yesterday.strftime('%Y-%m-%d')
 
 if __name__ == '__main__':
-    # get_top_stocks()
-    stocks = ['NVDA', 'BABA']
+    records = table.find_one({'agg_date': yesterday_str})
+    # stocks = ['NVDA', 'BABA']
+    stocks = [record['ticker'] for record in records[:10]]
     for stock in stocks:
         print(f"{time.time()}: {stock}")
         subprocess.run(
-            ["/Users/zhangbin/workspace/TradingAgents/.venv/bin/python",
-             "/Users/zhangbin/workspace/TradingAgents/one.py",
+            ["/data/www/TradingAgents/.venv/bin/python",
+             "/data/www/TradingAgents/one.py",
              stock])
